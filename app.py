a63e5018b0c79889be2fe266515ea9336d509a0c
Franklin Adjei <57026567+hotlynnn@users.noreply.github.com>
2022-08-18 22:07:55 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,240 @@
+"""
+Application for entering pricing data and displaying basic analytics
+"""
+# Built-ins
+import datetime
+from turtle import update
+
+import dash_bootstrap_components as dbc
+
+# 3rd-party
+import plotly.express as px
+from dash import Dash, Input, Output, State, ctx, dash_table, dcc, html
+
+# Internal
+from cpi import Observation
+
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+
+app.layout = html.Div(
+    [
+        html.H1("Data Entry Dashboard", id="header"),
+        html.Div(
+            [
+                html.Div(
+                    children=[
+                        html.H1("Price Observation Data Entry"),
+                        html.Br(),
+                        html.Label("Date"),
+                        dcc.DatePickerSingle(date=datetime.date.today(), id="date-input"),
+                        html.Br(),
+                        html.Label("Category"),
+                        dcc.Dropdown(
+                            options=Observation.available_categories(),
+                            value="Food",
+                            id="category-input",
+                        ),
+                        html.Br(),
+                        html.Label("Item"),
+                        dcc.Dropdown(
+                            options=Observation.available_items(),
+                            value="USDA Grade-A eggs",
+                            id="item-input",
+                        ),
+                        html.Br(),
+                        html.Label("Price"),
+                        dbc.Input(type="number", id="price-input", min=0, step=0.01),
+                        html.Br(),
+                        html.Label("State"),
+                        dcc.Dropdown(
+                            options=Observation.available_states(),
+                            value="Texas",
+                            id="state-input",
+                        ),
+                        html.Br(),
+                        html.Label("City"),
+                        dcc.Dropdown(
+                            options=Observation.available_cities(),
+                            value="Dallas",
+                            id="city-input",
+                        ),
+                        html.Br(),
+                        dbc.Button(
+                            "Save Observation",
+                            color="success",
+                            className="me-1",
+                            id="save-button",
+                            n_clicks=0,
+                        ),
+                        html.Br(),
+                        html.Br(),
+                        html.Label("Number of Matching Observations to delete"),
+                        dbc.Input(type="number", value=1, id="delete-n-observations"),
+                        html.Br(),
+                        html.Label("Delete Most Recent First?"),
+                        dbc.Checklist(
+                            options=[
+                                {"label": "Yes"},
+                            ],
+                            id="delete-most-recent-toggle",
+                        ),
+                        html.Br(),
+                        dbc.Button(
+                            "Delete Matching Observations",
+                            color="danger",
+                            className="me-1",
+                            id="delete-button",
+                            n_clicks=0,
+                        ),
+                    ],
+                    style={"padding": 10, "flex": 1},
+                ),
+                html.Div(
+                    children=[
+                        html.Label("Graph Type"),
+                        dcc.Dropdown(
+                            options=[
+                                "Item Prices Over Time",
+                                "Average Item Price by City",
+                            ],
+                            value="Item Prices Over Time",
+                            id="graph-type",
+                        ),
+                        dcc.Graph(
+                            figure=px.scatter(
+                                Observation.table_df(),
+                                x="Date",
+                                y="Price",
+                                color="Item",
+                                title="Item Prices Over Time",
+                            ),
+                            id="observation-graph",
+                        ),
+                        html.H3("Table of Observations", style={"text-align": "center"}),
+                        dash_table.DataTable(
+                            Observation.table_df().to_dict("records"),
+                            id="observation-table",
+                        ),
+                    ],
+                    style={"padding": 10, "flex": 1},
+                ),
+            ],
+            style={"display": "flex", "flex-direction": "row"},
+        ),
+    ],
+)
+
+
+@app.callback(
+    Output(component_id="observation-table", component_property="data"),
+    Output(component_id="observation-graph", component_property="figure"),
+    Input(component_id="save-button", component_property="n_clicks"),
+    Input(component_id="delete-button", component_property="n_clicks"),
+    Input(component_id="graph-type", component_property="value"),
+    Input(component_id="date-input", component_property="date"),
+    State(component_id="category-input", component_property="value"),
+    State(component_id="item-input", component_property="value"),
+    State(component_id="price-input", component_property="value"),
+    State(component_id="state-input", component_property="value"),
+    State(component_id="city-input", component_property="value"),
+    State(component_id="delete-n-observations", component_property="value"),
+)
+def update_observation(
+    n_clicks: float,
+    n_clicks2,
+    graph_type: str,
+    date: str,
+    category: str,
+    item: str,
+    price: str,
+    state: str,
+    city: str,
+    observations_to_delete,
+    *args
+):
+    triggered_id = ctx.triggered_id  # Id of the input that triggered the callback
+
+    if triggered_id == "graph-type":
+        return update_graph(graph_type, date)
+
+    if triggered_id == "date-input":
+        if graph_type != "Average Item Price by City":
+            return update_graph(graph_type, date)
+        return update_graph("Average Item Price by City", date)
+
+    elif triggered_id == "delete-button":
+        return delete_observations(
+            n_clicks2=n_clicks2,
+            date=date,
+            category=category,
+            item=item,
+            price=price,
+            state=state,
+            city=city,
+            observations_to_delete=observations_to_delete,
+        )
+    else:
+        if n_clicks >= 1:
+            obj = Observation(
+                Date=datetime.datetime.strptime(date, "%Y-%m-%d").date(),
+                Category=category,
+                Item=item,
+                Price=float(price),
+                State=state,
+                City=city,
+            )
+            obj.write()
+        df = Observation.table_df()
+        return df.to_dict("records"), px.scatter(df, x="Date", y="Price", color="Item")
+
+
+def delete_observations(
+    n_clicks2, observations_to_delete, date, category, state, city, price, item
+):
+    """
+    Function to delete an Observation using the data from the entry.
+    """
+    if n_clicks2 >= 1:
+        number_to_delete = observations_to_delete
+        obj = Observation(
+            Date=datetime.datetime.strptime(date, "%Y-%m-%d").date(),
+            Category=category,
+            Item=item,
+            Price=float(price),
+            State=state,
+            City=city,
+        )
+        obj.delete_matching(
+            state=state,
+            city=city,
+            date=date,
+            category=category,
+            n_to_delete=number_to_delete,
+        )
+    df = Observation.table_df()
+    return df.to_dict("records"), px.scatter(df, x="Date", y="Price", color="Item")
+
+
+def update_graph(graph_type: str, date: str):
+    if graph_type == "Average Item Price by City":
+        df = Observation.table_df()
+        # The selected date is not represented the same way in the dataframe.
+        # Hence the selected date is modified to match that of the dataframe to
+        # enable filtering of the data.
+        date = str(datetime.datetime.strptime(date + " 00:00:00", "%Y-%m-%d %H:%M:%S"))
+        data_for_selected_date = df.loc[df["Date"] == date]
+        return df.to_dict("records"), px.bar(
+            data_for_selected_date.groupby(["City", "Item"]).mean().reset_index(),
+            x="Item",
+            y="Price",
+            color="City",
+            hover_data=["Item"],
+            barmode="group",
+        )
+    df = Observation.table_df()
+    return df.to_dict("records"), px.scatter(df, x="Date", y="Price", color="Item")
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)  # Runs at localhost:8050 by default
---
+++ cpi.py
@@ -0,0 +1,196 @@
+"""
+Library for modeling prices for various objects
+"""
+# Built-ins
+import datetime
+import functools
+import itertools
+import logging
+import random
+import sqlite3
+from typing import Optional, Union
+
+# 3rd-party
+import pandas as pd
+
+# Internal
+
+
+logger = logging.getLogger(__name__)
+db_file = "test.db"
+
+
+def sqlize(v: Union[str, int, float, bool, datetime.date, datetime.date]) -> str:
+    # This function is for converting values to SQL strings.
+    if isinstance(v, str):
+        v = v.replace("'", "''")
+    elif isinstance(v, datetime.datetime):
+        v = v.strftime("%Y-%m-%d %H:%M:%S.%f")
+    elif isinstance(v, datetime.date):
+        v = v.strftime("%Y-%m-%d")
+    elif isinstance(v, bool):
+        v = int(v)
+    return f"'{v}'" if isinstance(v, str) else str(v)
+
+
+class Observation:
+
+    Date: Optional[datetime.date] = None
+    Item: Optional[str] = None
+    Price: Optional[float] = None
+    Category: Optional[str] = None
+    State: Optional[str] = None
+    City: Optional[str] = None
+    AddedOn: datetime.datetime = datetime.datetime.now()
+
+    category_item_map = {
+        "Food": ["USDA Grade-A eggs, Dozen"],
+        "Fuel": ["Regular Gasoline, Gallon"],
+        "Clothing": ["Wool Socks, Pair"],
+    }
+    state_city_map = {
+        "California": ["Los Angelos", "San Francisco"],
+        "New York": ["New York City"],
+        "Texas": ["Austin", "Dallas"],
+    }
+    item_base_price = {
+        "USDA Grade-A eggs, Dozen": 2.99,
+        "Regular Gasoline, Gallon": 4.65,
+        "Wool Socks, Pair": 21.95,
+    }
+    state_price_mu_std = {
+        "California": (1.5, 0.15),
+        "New York": (1.75, 0.25),
+        "Texas": (1, 0.10),
+    }
+
+    @classmethod
+    @functools.lru_cache(maxsize=None)
+    def available_items(cls):
+        # This function returns a list of all available items from the
+        # category_item_map dictionary.
+        return list(itertools.chain.from_iterable(cls.category_item_map.values()))
+
+    @classmethod
+    @functools.lru_cache(maxsize=None)
+    def available_categories(cls):
+        # This function returns a list of all available categories from the
+        # category_item_map dictionary. The categories are the keys of the
+        # dictionary.
+        return list(cls.category_item_map.keys())
+
+    @classmethod
+    @functools.lru_cache(maxsize=None)
+    def available_states(cls):
+        return list(cls.state_city_map.keys())
+
+    @classmethod
+    @functools.lru_cache(maxsize=None)
+    def available_cities(cls):
+        # This function returns a list of all available cities from the state_city_map dictionary.
+        # The cities are the values of the dictionary, with the keys being the
+        # states.
+        return list(itertools.chain.from_iterable(cls.state_city_map.values()))
+
+    def __init__(self, **kwargs):
+        for k, v in kwargs.items():
+            try:
+                setattr(self, k, v)
+            except AttributeError:
+                logger.warning(
+                    f"{k} is not a valid attribute of Observation. Ignoring..."
+                )
+
+    def write(self):
+        with sqlite3.connect(db_file) as con:
+            # This is to write the observation to the database.
+            # Create a list of the SQL standard representations of the attributes of the observation.
+            # Then join the list together separated by comma to fill the various columns of the table.
+            row = [
+                sqlize(v)
+                for v in [
+                    self.Date,
+                    self.Item,
+                    self.Price,
+                    self.Category,
+                    self.State,
+                    self.City,
+                ]
+            ]
+            sql = (
+                f"INSERT INTO Observation (Date, Item, Price, Category, State, City) values "
+                f'({", ".join(row)})'
+            )
+            con.execute(sql)
+
+    @classmethod
+    def create_table(cls):
+        sql = """
+        create table Observation (
+            Date date not null,
+            Item text not null,
+            Price numeric not null,
+            Category text not null,
+            State text not null,
+            City text not null,
+            AddedOn datetime default current_timestamp
+        );
+        """
+        with sqlite3.connect(db_file) as con:
+            # First, check if the table exists. If it does, then delete it.
+            # Else, create the table.
+            con.execute("drop table if exists Observation")
+            con.execute(sql)
+            # Load test data
+            df = cls.get_test_data()
+            df.to_sql("Observation", con=con, if_exists="append", index=False)
+
+    @classmethod
+    def get_test_data(cls) -> pd.DataFrame:
+        dt_range = pd.date_range(
+            end=datetime.date.today(), periods=10, freq="D", inclusive="both"
+        )
+        combos = [
+            {"Date": dt, "Category": cat, "Item": item, "State": state, "City": city}
+            for cat, items in cls.category_item_map.items()
+            for item in items
+            for state, cities in cls.state_city_map.items()
+            for city in cities
+            for dt in dt_range
+            for _ in range(5)
+        ]  # Create 5 prices per combo
+        df = pd.DataFrame(combos)
+        # Set all base prices to the average price for the item
+        df["Price"] = df.apply(lambda s: cls.item_base_price[s["Item"]], axis=1)
+        # Multiply the base price by the state scaling factor
+        df["Price"] = df.apply(
+            lambda s: s["Price"] * cls.state_price_mu_std[s["State"]][0], axis=1
+        )
+        # Generate a gaussian price based on the state standard deviation
+        df["Price"] = df.apply(
+            lambda s: random.gauss(
+                s["Price"], s["Price"] * cls.state_price_mu_std[s["State"]][1]
+            ),
+            axis=1,
+        )
+        return df
+
+    @staticmethod
+    def table_df() -> pd.DataFrame:
+        with sqlite3.connect(db_file) as con:
+            sql = "select * from Observation"
+            return pd.read_sql(sql, con)
+
+    def delete_matching(
+        self, n_to_delete: int = 1, order_to_delete_in: Optional[dict] = None, **kwargs
+    ):
+        with sqlite3.connect(db_file) as con:
+            conditions = " AND ".join(
+                [f"{k.capitalize()}={sqlize(v)}" for k, v in kwargs.items()]
+            )
+            sql_to_delete_rows = f"DELETE from Observation WHERE rowid in (SELECT rowid FROM Observation WHERE {conditions} LIMIT {n_to_delete})"
+            con.execute(sql_to_delete_rows)
+
+
+if __name__ == "__main__":
+    pass
